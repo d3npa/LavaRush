c2db08715bed21278feee7e8526bdd8b8b073dd3
d3npa <kyo@tilde.pink>
2024-08-18 14:58:14 +0900

    add a dynamic slopeLimit and add minimum wave length for player experience

--- src/jp/d3npa/lavarush/LavaRush.java
+++ src/jp/d3npa/lavarush/LavaRush.java
@@ -25,6 +25,7 @@ public class LavaRush extends LavaAbility implements AddonAbility {
     final static String AUTHOR = "d3npa";
     final static String DESCRIPTION = "An offensive Lavabending ability. To use, left-click in any direction. " +
             "A wave of lava will erupt from the ground, swallowing up anything in its way.";
+    final static int MIN_LENGTH = 4;
 
     static ArrayList<LavaRush> instances = new ArrayList<>();
     static long cooldown = 10000;
@@ -76,11 +77,21 @@ public class LavaRush extends LavaAbility implements AddonAbility {
                 .add(new Vector(0, -1, 0)) // in the ground
                 .add(direction.clone().multiply(3)); // start 3 blocks in front of player
 
+        /* make sure origin is not in the air */
+        int counter = 0;
+        while (GeneralMethods.isTransparent(origin.getBlock())) {
+            origin.setY(origin.getY() - 1);
+            if (++counter == 3) {
+                this.remove();
+                return;
+            }
+        }
+
         lastProgress = System.currentTimeMillis();
         line = new BlockIterator(origin.getWorld(), origin.toVector(), direction, 0, range);
         calculateWave();
 
-        if (!wave.isEmpty()) {
+        if (wave.size() >= MIN_LENGTH) {
             state = LRState.ANIMATING;
             bendingPlayer.addCooldown(this);
             this.start();
@@ -124,7 +135,7 @@ public class LavaRush extends LavaAbility implements AddonAbility {
                     }
                 }
                 case RESTING -> {
-                    if (++spam == 30) {
+                    if (++spam == 60) {
                         state = LRState.CLEANUP;
                     }
                 }
@@ -199,6 +210,7 @@ public class LavaRush extends LavaAbility implements AddonAbility {
 
     public void calculateWave() {
         /* precalculate a list of all blocks transformed by the wave */
+        int slopeLimit = 1;
         while (line.hasNext()) {
             BlockFace directionAsBlockFace = GeneralMethods.getCardinalDirection(direction);
 
@@ -208,9 +220,12 @@ public class LavaRush extends LavaAbility implements AddonAbility {
 
             ArrayList<Block> layer = new ArrayList<>();
             for (Block block : new Block[] { left, middle, right }) {
-                block = getLavabendableBlock(block);
-                if (block != null) {
-                    layer.add(block);
+                Block lavabendable = getLavabendableBlock(block, slopeLimit);
+                if (lavabendable != null) {
+                    layer.add(lavabendable);
+                    if (lavabendable.getY() < block.getY()) {
+                        slopeLimit++;
+                    }
                 }
             }
 
@@ -238,15 +253,21 @@ public class LavaRush extends LavaAbility implements AddonAbility {
         };
     }
 
-    public Block getLavabendableBlock(Block block) {
+    public Block getLavabendableBlock(Block block, int slopeLimit) {
         /* some checks must be made on the block before adding
          *  - is there space above?
          *  - if it is air, is there earth below it? (slight slope)
          *  - if the block above is not air, does that one have air above and is earthbendable?
          *  - is it earthbendable?
          */
-        if (GeneralMethods.isTransparent(block)) {
-            /* when air or water, torch etc, use the block beneath and perform more checks */
+
+        /* when air or water, torch etc, use the block beneath and perform more checks */
+        int counter = 0;
+        while (GeneralMethods.isTransparent(block)) {
+            if (counter++ == Math.max(slopeLimit, 0)) {
+                return null;
+            }
+
             block = block.getRelative(BlockFace.DOWN);
         }
 
