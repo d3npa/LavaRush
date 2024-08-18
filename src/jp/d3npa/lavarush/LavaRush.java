ef3bff5ff9f46e2368caa0f867d4c0cb5d0abb40
d3npa <kyo@tilde.pink>
2024-08-18 12:00:49 +0900

    add blocks to the right and left of the line to the list

--- src/jp/d3npa/lavarush/LavaRush.java
+++ src/jp/d3npa/lavarush/LavaRush.java
@@ -32,7 +32,7 @@ public class LavaRush extends LavaAbility implements AddonAbility {
     private Location origin;
     private Vector direction;
     private int spam = 0;
-    private int interval = 100;
+    private int interval = 50;
     private long lastProgress;
     private LRState state;
 
@@ -72,7 +72,7 @@ public class LavaRush extends LavaAbility implements AddonAbility {
         origin = player
                 .getLocation()
                 .add(new Vector(0, -1, 0)) // in the ground
-                .add(direction.clone().multiply(2)); // start 2 blocks in front of player
+                .add(direction.clone().multiply(3)); // start 3 blocks in front of player
 
         bendingPlayer.addCooldown(this);
         lastProgress = System.currentTimeMillis();
@@ -173,7 +173,7 @@ public class LavaRush extends LavaAbility implements AddonAbility {
 
     public void calculateWave() {
         /* use player position to precalculate a list of all blocks transformed by the wave */
-        final int range = 8;
+        final int range = 10;
         depth = 0;
 
         ProjectKorra.log.info(String.format("Origin: %s", origin.toString()));
@@ -181,35 +181,62 @@ public class LavaRush extends LavaAbility implements AddonAbility {
 
         for (int i = 0; i < range; i++) {
             // get the next blocks in the wave
-
             Vector modifiedDirection = direction.clone().multiply(i);
-            Location middle = origin.clone().add(modifiedDirection);
-
-            Block block = middle.getBlock();
-            /* some checks must be made on the block before adding
-             *  - is there space above?
-             *  - if it is air, is there earth below it? (slight slope)
-             *  - is it earthbendable?
-             */
-            if (GeneralMethods.isTransparent(block)) {
-                /* when air or water, torch etc, use the block beneath and perform more checks */
-                block = block.getRelative(BlockFace.DOWN);
-            }
-
-            Block above = block.getRelative(BlockFace.UP);
-            if (!GeneralMethods.isTransparent(above)) {
-                // dont worry about going up slopes for now.
-                break;
-            }
-
-            if (!isEarthbendable(block)) {
-                break;
-            }
+            Location middleLocation = origin.clone().add(modifiedDirection);
+            Block middle = middleLocation.getBlock();
+            BlockFace directionAsBlockFace = GeneralMethods.getCardinalDirection(direction);
+            Block left = middle.getRelative(getLeftBlockFace(directionAsBlockFace));
+            Block right = middle.getRelative(getLeftBlockFace(directionAsBlockFace).getOppositeFace());
 
             ArrayList<Block> layer = new ArrayList<>();
-            layer.add(block);
+
+            for (Block block : new Block[] { left, middle, right }) {
+
+                /* some checks must be made on the block before adding
+                 *  - is there space above?
+                 *  - if it is air, is there earth below it? (slight slope)
+                 *  - is it earthbendable?
+                 */
+                if (GeneralMethods.isTransparent(block)) {
+                    /* when air or water, torch etc, use the block beneath and perform more checks */
+                    block = block.getRelative(BlockFace.DOWN);
+                }
+
+                Block above = block.getRelative(BlockFace.UP);
+                if (!GeneralMethods.isTransparent(above)) {
+                    // don't worry about going up slopes for now.
+                    break;
+                }
+
+                if (!isEarthbendable(block)) {
+                    break;
+                }
+
+                layer.add(block);
+            }
+
+            if (layer.isEmpty()) {
+                // no blocks could be bent, so stop the wave here.
+                return;
+            }
+
             wave.add(layer);
         }
+    }
 
+
+    public BlockFace getLeftBlockFace(BlockFace forward)
+    {
+        return switch (forward) {
+            case NORTH -> BlockFace.WEST;
+            case SOUTH -> BlockFace.EAST;
+            case WEST -> BlockFace.SOUTH;
+            case EAST -> BlockFace.NORTH;
+            case NORTH_WEST -> BlockFace.SOUTH_WEST;
+            case NORTH_EAST -> BlockFace.NORTH_WEST;
+            case SOUTH_WEST -> BlockFace.SOUTH_EAST;
+            case SOUTH_EAST -> BlockFace.NORTH_EAST;
+            default -> null;
+        };
     }
 }
